8ffd4de36f2a2894c3b13eef0a22c10d06e49c4a
yakovb <yakov.boglev@gmail.com>
2015-07-01 14:26:53 +0100

    Initial commit - simple inverter plugin Shows that using Spire and functional approach is possible for simple pixel point operations

---
+++ src/main/scala/plugins/Inverter_.scala
@@ -0,0 +1,28 @@
+import ij.plugin.filter.PlugInFilter
+import ij.plugin.filter.PlugInFilter._
+import ij.process.ImageProcessor
+import ij.{IJ, ImagePlus}
+
+import spire.algebra.Ring
+import spire.syntax.ring._
+import spire.std.any._
+
+import scala.collection.parallel.mutable.ParArray
+
+class Inverter_ extends PlugInFilter {
+  override def setup(arg: String, imp: ImagePlus): Int =
+    if (arg == "about") {showAbout(); DONE}
+    else DOES_8G + DOES_STACKS + SUPPORTS_MASKING
+
+  override def run(ip: ImageProcessor): Unit = {
+    val pixels = ip.getPixels.asInstanceOf[Array[Byte]].par
+    ip.setPixels(inv(inv(inv(pixels))).toArray)
+  }
+
+  private def showAbout(): Unit =
+    IJ.showMessage("this is my scala-based inverter")
+
+  private def inv[@specialized(Byte,Int) A: Ring](ar: ParArray[A]): ParArray[A] = {
+    ar map (255 - _)
+  }
+}
