eee47410b856639b3d7a0ba1b43b927e39a92d2e
yakovb <yakov.boglev@gmail.com>
2015-08-09 12:13:21 +0100

    Change Operation structure  base trait is Operation, subclassed into PointOperation and will do another for Filters. Idea is to have Image classes accept Operations as arguments

--- src/main/scala/operations/Operation.scala
+++ src/main/scala/operations/Operation.scala
@@ -1,14 +1,16 @@
 package operations
 
-sealed trait Operation {
+sealed trait Operation
+
+sealed trait PointOperation extends Operation {
   def run(pixel: Int): Int
 }
 
-case class PointOp(f: Int => Int) extends Operation {
+case class PointOp(f: Int => Int) extends PointOperation {
   override def run(pixel: Int): Int = f(pixel)
 }
 
-case class PointOpRGB(redOp: Int => Int, greenOp: Int => Int, blueOp: Int => Int) extends Operation {
+case class PointOpRGB(redOp: Int => Int, greenOp: Int => Int, blueOp: Int => Int) extends PointOperation {
   override def run(pixel: Int): Int = {
     val red = redOp ((pixel >> 16) & 0xff)
     val green = greenOp ((pixel >> 8) & 0xff)
