0cee1c611d3b7854f77d6c41e54c6241d5b46f0f
yakovb <yakov.boglev@gmail.com>
2015-08-09 18:55:57 +0100

    Updated plugins to match new Image/Operation structure

--- src/main/scala/plugins/Inverter_.scala
+++ src/main/scala/plugins/Inverter_.scala
@@ -2,18 +2,19 @@ import ij.ImagePlus
 import ij.plugin.filter.PlugInFilter
 import ij.plugin.filter.PlugInFilter._
 import ij.process.ImageProcessor
-import images.Image
+import images.ParImage
 import operations.PointOp
 
 class Inverter_ extends PlugInFilter {
   override def setup(arg: String, imp: ImagePlus): Int =
-    DOES_8G + DOES_STACKS + SUPPORTS_MASKING + NO_CHANGES
+    DOES_8G + DOES_STACKS + SUPPORTS_MASKING
 
   override def run(ip: ImageProcessor): Unit = {
-    val pixels = ip.getPixels.asInstanceOf[Array[Int]]
-    val image = Image.makeImageInt(pixels, ip.getWidth, ip.getHeight)
-    val step1 = image.processPoints( PointOp(255 - _) )
-
-    ip.setPixels(step1.matrix.toArray)
+    val pixels = ip.getPixels.asInstanceOf[Array[Byte]]
+    val image = ParImage[Byte](pixels.par, ip.getWidth, ip.getHeight)
+    val transformedImage = image.traversePoints[Byte](
+      PointOp[Byte,Byte](
+        (p:Byte) => (255 - p).toByte ) )
+    ip.setPixels(transformedImage.matrix.toArray)
   }
 }
--- src/main/scala/plugins/RGB_to_Grey.scala
+++ src/main/scala/plugins/RGB_to_Grey.scala
@@ -1,8 +1,8 @@
 import ij.ImagePlus
 import ij.plugin.filter.PlugInFilter
 import ij.plugin.filter.PlugInFilter._
-import ij.process.{ColorProcessor, ImageProcessor}
-import images.Image
+import ij.process.{ByteProcessor, ImageProcessor}
+import images.ParImage
 import operations.PointOpRGB
 
 class RGB_to_Grey extends PlugInFilter {
@@ -11,14 +11,14 @@ class RGB_to_Grey extends PlugInFilter {
 
   override def run(ip: ImageProcessor): Unit = {
     val pixels = ip.getPixels.asInstanceOf[Array[Int]]
-    val image = Image.makeImageInt(pixels, ip.getWidth, ip.getHeight)
-    val step1 = image.processPoints(
-      PointOpRGB(
-        (r: Int) => (r * .2126).toInt,
-        (g: Int) => (g * .7152).toInt,
-        (b: Int) => (b * .0722).toInt ) )
+    val image = ParImage(pixels.par, ip.getWidth, ip.getHeight)
 
-    val result = new ColorProcessor(step1.width, step1.height, step1.matrix.toArray)
+    val transformedImage = image.traversePoints[Byte](
+      PointOpRGB[Double,Byte]
+        (_ * 0.2126, _ * 0.7152, _ * 0.0722)
+        ((r,g,b) => (r+g+b).toByte) )
+
+    val result = new ByteProcessor(transformedImage.width, transformedImage.height, transformedImage.matrix.toArray)
     new ImagePlus("grey pic", result) show
   }
 }
