4280c322ce45317394685a2eab6a8239b91238e7
yakovb <yakov.boglev@gmail.com>
2015-08-09 14:56:06 +0100

    updated RGB to Grey plugin

--- src/main/scala/plugins/RGB_to_Grey.scala
+++ src/main/scala/plugins/RGB_to_Grey.scala
@@ -1,29 +1,24 @@
 import ij.ImagePlus
 import ij.plugin.filter.PlugInFilter
 import ij.plugin.filter.PlugInFilter._
-import ij.process.{ByteProcessor, ImageProcessor}
-
-import scala.collection.parallel.mutable.ParArray
+import ij.process.{ColorProcessor, ImageProcessor}
+import images.Image
+import operations.PointOpRGB
 
 class RGB_to_Grey extends PlugInFilter {
   override def setup(arg: String, imp: ImagePlus): Int =
     DOES_RGB + DOES_STACKS + SUPPORTS_MASKING
 
   override def run(ip: ImageProcessor): Unit = {
-    val w = ip.getWidth
-    val h = ip.getHeight
+    val pixels = ip.getPixels.asInstanceOf[Array[Int]]
+    val image = Image.makeImageInt(pixels, ip.getWidth, ip.getHeight)
+    val step1 = image.processPoints(
+      PointOpRGB(
+        (r: Int) => (r * .2126).toInt,
+        (g: Int) => (g * .7152).toInt,
+        (b: Int) => (b * .0722).toInt ) )
 
-    val pixels = ip.getPixels.asInstanceOf[Array[Int]].par
-    val greyProcessor = new ByteProcessor(w, h, convert(pixels).toArray)
-    new ImagePlus("grey pic", greyProcessor) show
-  }
-
-  private def convert(ar: ParArray[Int]): ParArray[Byte] = {
-    ar map (p => {
-      val red = ((p >> 16) & 255) * 0.2126
-      val green = ((p >> 8) & 255) * 0.7152
-      val blue = (p & 255) * 0.0722
-      (red + green + blue).toByte
-    })
+    val result = new ColorProcessor(step1.width, step1.height, step1.matrix.toArray)
+    new ImagePlus("grey pic", result) show
   }
 }
