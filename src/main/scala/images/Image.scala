e1f95219fbaabb86cd0e57b79770eeb3fd5e86d2
yakovb <yakov.boglev@gmail.com>
2015-08-09 12:39:44 +0100

    Added processPoints method Iterates over pixels and applies the operation. Caller specifies output type via String

--- src/main/scala/images/Image.scala
+++ src/main/scala/images/Image.scala
@@ -1,26 +1,44 @@
 package images
 
+import operations.PointOperation
+
 import scala.collection.parallel.mutable.ParArray
 
 sealed trait Image {
   val width: Int
   val height: Int
   val matrix: ParArray[Int]
+
+  def processPoints(op: PointOperation)(output: String): Image = {
+    val newMatrix = for (pixel <- matrix)
+      yield op runOn pixel
+    output match {
+      case "RGB" => ImageRGB(newMatrix, width, height)
+      case "Grey" => ImageGrey(newMatrix, width, height)
+      case "Binary" => ImageBinary(newMatrix, width, height)
+    }
+  }
 }
 
-case class ImageRGB(pixels: Array[Int], w: Int, h: Int) extends Image {
+case class ImageRGB(pixels: ParArray[Int], w: Int, h: Int) extends Image {
+  def this(pixels: Array[Int], w: Int, h: Int) = this(pixels.par, w, h)
+
+  override val width: Int = w
+  override val height: Int = h
+  override val matrix: ParArray[Int] = pixels
+}
+
+case class ImageGrey(pixels: ParArray[Int], w: Int, h: Int) extends Image {
+  def this(pixels: Array[Int], w: Int, h: Int) = this(pixels.par, w, h)
+
   override val width: Int = w
   override val height: Int = h
   override val matrix: ParArray[Int] = pixels.par
 }
 
-case class ImageGrey(pixels: Array[Int], w: Int, h: Int) extends Image {
-  override val width: Int = w
-  override val height: Int = h
-  override val matrix: ParArray[Int] = pixels.par
-}
+case class ImageBinary(pixels: ParArray[Int], w: Int, h: Int) extends Image {
+  def this(pixels: Array[Int], w: Int, h: Int) = this(pixels.par, w, h)
 
-case class ImageBinary(pixels: Array[Int], w: Int, h: Int) extends Image {
   override val width: Int = w
   override val height: Int = h
   override val matrix: ParArray[Int] = pixels.par
--- src/main/scala/operations/Operation.scala
+++ src/main/scala/operations/Operation.scala
@@ -3,15 +3,15 @@ package operations
 sealed trait Operation
 
 sealed trait PointOperation extends Operation {
-  def run(pixel: Int): Int
+  def runOn(pixel: Int): Int
 }
 
 case class PointOp(f: Int => Int) extends PointOperation {
-  override def run(pixel: Int): Int = f(pixel)
+  override def runOn(pixel: Int): Int = f(pixel)
 }
 
 case class PointOpRGB(redOp: Int => Int, greenOp: Int => Int, blueOp: Int => Int) extends PointOperation {
-  override def run(pixel: Int): Int = {
+  override def runOn(pixel: Int): Int = {
     val red = redOp ((pixel >> 16) & 0xff)
     val green = greenOp ((pixel >> 8) & 0xff)
     val blue = blueOp (pixel & 0xff)
