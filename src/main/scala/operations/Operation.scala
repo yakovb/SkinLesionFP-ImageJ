b282b1860ddfc5b51c6e65ee4b213db8c937fe7d
yakovb <yakov.boglev@gmail.com>
2015-08-09 11:39:37 +0100

    method name change

--- src/main/scala/operations/Operation.scala
+++ src/main/scala/operations/Operation.scala
@@ -8,7 +8,7 @@ case class PointOp(f: Int => Int) extends Operation {
   override def run(pixel: Int): Int = f(pixel)
 }
 
-case class PointOpColour(redOp: Int => Int, greenOp: Int => Int, blueOp: Int => Int) extends Operation {
+case class PointOpRGB(redOp: Int => Int, greenOp: Int => Int, blueOp: Int => Int) extends Operation {
   override def run(pixel: Int): Int = {
     val red = redOp ((pixel >> 16) & 0xff)
     val green = greenOp ((pixel >> 8) & 0xff)
