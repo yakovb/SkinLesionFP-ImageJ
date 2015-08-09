7d0366c36547516e3be94c5dcab2d95f3686ddf1
yakovb <yakov.boglev@gmail.com>
2015-08-09 11:55:29 +0100

    added ImageJ case class

--- src/main/scala/images/Image.scala
+++ src/main/scala/images/Image.scala
@@ -7,3 +7,9 @@ sealed trait Image {
   val height: Int
   val matrix: ParArray[Int]
 }
+
+case class ImageIJ(pixels: Array[Int], w: Int, h: Int) extends Image {
+  override val width: Int = w
+  override val height: Int = h
+  override val matrix: ParArray[Int] = pixels.par
+}
