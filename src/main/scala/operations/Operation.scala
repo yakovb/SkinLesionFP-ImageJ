3c034388dc32ec9175dbd32536ef1b5ff7723d4d
yakovb <yakov.boglev@gmail.com>
2015-08-09 11:35:05 +0100

    Added colour point op

--- src/main/scala/operations/Operation.scala
+++ src/main/scala/operations/Operation.scala
@@ -7,3 +7,12 @@ sealed trait Operation {
 case class PointOp(f: Int => Int) extends Operation {
   override def run(pixel: Int): Int = f(pixel)
 }
+
+case class PointOpColour(redOp: Int => Int, greenOp: Int => Int, blueOp: Int => Int) extends Operation {
+  override def run(pixel: Int): Int = {
+    val red = redOp ((pixel >> 16) & 0xff)
+    val green = greenOp ((pixel >> 8) & 0xff)
+    val blue = blueOp (pixel & 0xff)
+    (red << 16) + (green << 8) + blue
+  }
+}
