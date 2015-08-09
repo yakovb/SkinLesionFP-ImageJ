6ebae2bc787b39b397a2cc1b16210aeaa8435f14
yakovb <yakov.boglev@gmail.com>
2015-08-09 12:44:38 +0100

    Changed Image hierarchy No point in constantly specifying output type as RGB, Grey etc since the underlying data is always an Int. Better to specify the output type as Int/Double etc

--- src/main/scala/images/Image.scala
+++ src/main/scala/images/Image.scala
@@ -9,37 +9,17 @@ sealed trait Image {
   val height: Int
   val matrix: ParArray[Int]
 
-  def processPoints(op: PointOperation)(output: String): Image = {
+  def processPoints(op: PointOperation): Image = {
     val newMatrix = for (pixel <- matrix)
       yield op runOn pixel
-    output match {
-      case "RGB" => ImageRGB(newMatrix, width, height)
-      case "Grey" => ImageGrey(newMatrix, width, height)
-      case "Binary" => ImageBinary(newMatrix, width, height)
-    }
+    ImageInt(newMatrix, width, height)
   }
 }
 
-case class ImageRGB(pixels: ParArray[Int], w: Int, h: Int) extends Image {
+case class ImageInt(pixels: ParArray[Int], w: Int, h: Int) extends Image {
   def this(pixels: Array[Int], w: Int, h: Int) = this(pixels.par, w, h)
 
   override val width: Int = w
   override val height: Int = h
   override val matrix: ParArray[Int] = pixels
-}
-
-case class ImageGrey(pixels: ParArray[Int], w: Int, h: Int) extends Image {
-  def this(pixels: Array[Int], w: Int, h: Int) = this(pixels.par, w, h)
-
-  override val width: Int = w
-  override val height: Int = h
-  override val matrix: ParArray[Int] = pixels.par
-}
-
-case class ImageBinary(pixels: ParArray[Int], w: Int, h: Int) extends Image {
-  def this(pixels: Array[Int], w: Int, h: Int) = this(pixels.par, w, h)
-
-  override val width: Int = w
-  override val height: Int = h
-  override val matrix: ParArray[Int] = pixels.par
 }
