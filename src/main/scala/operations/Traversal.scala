aa43409c2516375d188e74ebdff8131a71e9c35e
yakovb <yakov.boglev@gmail.com>
2015-08-19 18:17:51 +0100

    Changed params

--- src/main/scala/operations/Traversal.scala
+++ src/main/scala/operations/Traversal.scala
@@ -6,7 +6,8 @@ import scala.collection.parallel.mutable.ParArray
 
 sealed trait Traversal
 
-case class PointTraverse[A,B](im: Image[A], pointOp: PointOperation[A,B]) extends Traversal {
-  def traverse: ParArray[B] =
+case class PointTraverse() extends Traversal {
+
+  def traverse[A,B](im: Image[A], pointOp: PointOperation[A,B]): ParArray[B] =
     for (pixel <- im.matrix) yield pointOp runOn pixel
 }
