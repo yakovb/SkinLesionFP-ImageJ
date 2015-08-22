8cd2128528d4dc26a82ec6edfe9bb788fc47b123
yakovb <yakov.boglev@gmail.com>
2015-08-22 10:59:27 +0100

    Neighbourhood traversal moved from Image to Traversal

--- src/main/scala/images/Image.scala
+++ src/main/scala/images/Image.scala
@@ -1,36 +1,11 @@
 package images
 
-import operations.{NeighbourhoodOperation, PointOperation}
-
-import scala.collection.immutable
 import scala.collection.parallel.mutable.ParArray
 
 sealed trait Image[A] {
   val width: Int
   val height: Int
   val matrix: ParArray[A]
-
-  def traversePoints[B](op: PointOperation[A,B]): Image[B] = {
-    val newMatrix = for (pixel <- matrix)
-      yield op runOn pixel
-    
-    ParImage(newMatrix, width, height)
-  }
-
-  def traverseNeighbourhoods[B](op: NeighbourhoodOperation[A,B]): Image[B] = {
-    val allNeighbourhoods: immutable.IndexedSeq[immutable.IndexedSeq[A]] =
-    for {
-      row <- 1 until (height-1)
-      col <- 1 until (width-1)
-    } yield
-      for {
-        u <- -1 to 1
-        v <- -1 to 1
-      } yield
-        matrix(width * (row + u) + (col + v))
-
-    ParImage((allNeighbourhoods map (n => op runOn n.toList)).toParArray, width-1, height-1)
-  }
 }
 
 case class ParImage[A](pixels: ParArray[A], w: Int, h: Int) extends Image[A] {
--- src/main/scala/operations/Traversal.scala
+++ src/main/scala/operations/Traversal.scala
@@ -1,7 +1,8 @@
 package operations
 
-import images.Image
+import images.{ParImage, Image}
 
+import scala.collection.immutable
 import scala.collection.parallel.mutable.ParArray
 
 sealed trait Traversal
@@ -10,4 +11,24 @@ case class PointTraverse() extends Traversal {
 
   def traverse[A,B](im: Image[A], pointOp: PointOperation[A,B]): ParArray[B] =
     for (pixel <- im.matrix) yield pointOp runOn pixel
+}
+
+case class NeighbourTraverse() extends Traversal {
+
+  def traverse[A,B](im: Image[A], op: NeighbourhoodOperation[A,B]): Image[B] = {
+
+    val allNeighbourhoods: immutable.IndexedSeq[immutable.IndexedSeq[A]] =
+      for {
+        row <- 1 until (im.height-1)
+        col <- 1 until (im.width-1)
+      } yield
+      for {
+        u <- -1 to 1
+        v <- -1 to 1
+      } yield
+      im.matrix(im.width * (row + u) + (col + v))
+
+    ParImage((allNeighbourhoods map (n => op runOn n.toList)).toParArray, im.width-1, im.height-1)
+  }
+
 }
