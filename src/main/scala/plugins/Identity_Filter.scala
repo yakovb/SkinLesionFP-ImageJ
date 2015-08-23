37fbf92e97100ad28f9e0a606fe5435187c33aad
yakovb <yakov.boglev@gmail.com>
2015-08-23 11:56:58 +0100

    ID filter now works with new Transformation case class

--- src/main/scala/plugins/Identity_Filter.scala
+++ src/main/scala/plugins/Identity_Filter.scala
@@ -3,7 +3,7 @@ import ij.plugin.filter.PlugInFilter
 import ij.plugin.filter.PlugInFilter._
 import ij.process.{ByteProcessor, ImageProcessor}
 import images.ParImage
-import operations.NeighbourhoodOp
+import operations.{Funcs, NeighbourTraverse, TransformNeighbourhood}
 
 class Identity_Filter extends PlugInFilter {
   override def setup(arg: String, imp: ImagePlus): Int =
@@ -12,12 +12,9 @@ class Identity_Filter extends PlugInFilter {
   override def run(ip: ImageProcessor): Unit = {
     val pixels = ip.getPixels.asInstanceOf[Array[Byte]]
     val image = ParImage[Byte](pixels.par, ip.getWidth, ip.getHeight)
-    val transformedImage = image.traverseNeighbourhoods[Byte](
-      NeighbourhoodOp[Byte,Int,Byte](
-        (pairs: List[(Byte,Int)]) => (pairs map (p => p._1 * p._2)).sum.toByte)
-        (List(0,0,0,0,1,0,0,0,0)
-      )
-    )
+
+    val transformedImage = TransformNeighbourhood[Byte,Byte](image, NeighbourTraverse(), Funcs.id_filter) transform
+
     val result = new ByteProcessor(transformedImage.width - 1, transformedImage.height - 1, transformedImage.matrix.toArray)
     new ImagePlus("grey pic", result) show()
   }
