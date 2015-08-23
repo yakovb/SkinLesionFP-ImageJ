591f768a5685181ed5fd52ab775c1a2b72ac3ef4
yakovb <yakov.boglev@gmail.com>
2015-08-23 13:03:12 +0100

    Added crop border action

--- src/main/scala/plugins/Identity_Filter.scala
+++ src/main/scala/plugins/Identity_Filter.scala
@@ -3,7 +3,7 @@ import ij.plugin.filter.PlugInFilter
 import ij.plugin.filter.PlugInFilter._
 import ij.process.{ByteProcessor, ImageProcessor}
 import images.ParImage
-import operations.{Funcs, NeighbourTraverse, TransformNeighbourhood}
+import operations.{BorderAction, Funcs, NeighbourTraverse, TransformNeighbourhood}
 
 class Identity_Filter extends PlugInFilter {
   override def setup(arg: String, imp: ImagePlus): Int =
@@ -13,8 +13,10 @@ class Identity_Filter extends PlugInFilter {
     val pixels = ip.getPixels.asInstanceOf[Array[Byte]]
     val image = ParImage[Byte](pixels.par, ip.getWidth, ip.getHeight)
 
-    val transformedImage = TransformNeighbourhood[Byte,Byte](image, NeighbourTraverse(), Funcs.id_filter) transform
+    val transformedImage = TransformNeighbourhood[Byte,Byte](image, NeighbourTraverse(), Funcs.id_filter)(BorderAction.Crop) transform
 
+    println("w: "+ transformedImage.width + "; h: " + transformedImage.height + "; length: "+
+    transformedImage.width * transformedImage.height+ "; matrix size: "+ transformedImage.matrix.size)
     val result = new ByteProcessor(transformedImage.width - 1, transformedImage.height - 1, transformedImage.matrix.toArray)
     new ImagePlus("grey pic", result) show()
   }
