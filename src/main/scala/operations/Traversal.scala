a5134c8fc364d6571fc8a37d4dabaca7b938193c
yakovb <yakov.boglev@gmail.com>
2015-08-23 12:15:57 +0100

    Traversals return ParArrays rather than specific Image types

--- src/main/scala/operations/Traversal.scala
+++ src/main/scala/operations/Traversal.scala
@@ -1,35 +1,36 @@
 package operations
 
-import images.{Image, ParImage}
+import images.Image
 
-import scala.collection.immutable
+import scala.collection.parallel.mutable.ParArray
 
 sealed trait Traversal
 
 case class PointTraverse() extends Traversal {
 
-  def traverse[A,B](im: Image[A], pointOp: PointOperation[A,B]): Image[B] = {
-    val newMat = for (pixel <- im.matrix) yield pointOp runOn pixel
-    ParImage(newMat, im.width, im.height)
-  }
+  def traverse[A,B](im: Image[A], pointOp: PointOperation[A,B]): ParArray[B] =
+    for (pixel <- im.matrix) yield pointOp runOn pixel
+
 }
 
 case class NeighbourTraverse() extends Traversal {
 
-  def traverse[A,B](im: Image[A], op: NeighbourhoodOperation[A,B]): Image[B] = {
+  def traverse[A,B](im: Image[A], op: NeighbourhoodOperation[A,B]): ParArray[B] = {
 
-    val allNeighbourhoods: immutable.IndexedSeq[immutable.IndexedSeq[A]] =
+    val allNeighbourhoods =
       for {
         row <- 1 until (im.height-1)
         col <- 1 until (im.width-1)
       } yield
-      for {
-        u <- -1 to 1
-        v <- -1 to 1
-      } yield
-      im.matrix(im.width * (row + u) + (col + v))
 
-    ParImage((allNeighbourhoods map (n => op runOn n.toList)).toParArray, im.width-1, im.height-1)
+        (for {
+          u <- -1 to 1
+          v <- -1 to 1
+        } yield
+          im.matrix(im.width * (row + u) + (col + v))).toList
+
+    val newMat = allNeighbourhoods map ((n: List[A]) => op runOn n)
+    newMat.toParArray
   }
 
 }
