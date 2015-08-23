134f647f91156400ab8974cc35f487032898577f
yakovb <yakov.boglev@gmail.com>
2015-08-23 12:39:46 +0100

    Added Gauss Blur plugin

---
+++ src/main/scala/plugins/Gaussian_Blur.scala
@@ -0,0 +1,22 @@
+import ij.ImagePlus
+import ij.plugin.filter.PlugInFilter
+import ij.plugin.filter.PlugInFilter._
+import ij.process.{ByteProcessor, ImageProcessor}
+import images.ParImage
+import operations.{Funcs, NeighbourTraverse, TransformNeighbourhood}
+
+class Gaussian_Blur extends PlugInFilter {
+  override def setup(arg: String, imp: ImagePlus): Int =
+    DOES_8G + DOES_STACKS + SUPPORTS_MASKING
+
+  override def run(ip: ImageProcessor): Unit = {
+    val pixels = ip.getPixels.asInstanceOf[Array[Byte]]
+    val image = ParImage[Byte](pixels.par, ip.getWidth, ip.getHeight)
+
+    val transformedImage = TransformNeighbourhood[Byte,Byte](image, NeighbourTraverse(), Funcs.gaussBlur) transform
+
+    val result = new ByteProcessor(transformedImage.width - 1, transformedImage.height - 1, transformedImage.matrix.toArray)
+    new ImagePlus("grey pic", result) show()
+  }
+
+}
