bd3136f505e086bc556346d742d46c3cb3d62538
yakovb <yakov.boglev@gmail.com>
2015-08-23 12:18:36 +0100

    updated to use Transformation

--- src/main/scala/plugins/RGB_to_Grey.scala
+++ src/main/scala/plugins/RGB_to_Grey.scala
@@ -3,7 +3,7 @@ import ij.plugin.filter.PlugInFilter
 import ij.plugin.filter.PlugInFilter._
 import ij.process.{ByteProcessor, ImageProcessor}
 import images.ParImage
-import operations.PointOpRGB
+import operations.{Funcs, PointTraverse, TransformSimple}
 
 class RGB_to_Grey extends PlugInFilter {
   override def setup(arg: String, imp: ImagePlus): Int =
@@ -13,10 +13,7 @@ class RGB_to_Grey extends PlugInFilter {
     val pixels = ip.getPixels.asInstanceOf[Array[Int]]
     val image = ParImage(pixels.par, ip.getWidth, ip.getHeight)
 
-    val transformedImage = image.traversePoints[Byte](
-      PointOpRGB[Double,Byte]
-        (_ * 0.2126, _ * 0.7152, _ * 0.0722)
-        ((r,g,b) => (r+g+b).toByte) )
+    val transformedImage = TransformSimple[Int,Byte](image, PointTraverse(), Funcs.rgb_2_grey) transform
 
     val result = new ByteProcessor(transformedImage.width, transformedImage.height, transformedImage.matrix.toArray)
     new ImagePlus("grey pic", result) show
