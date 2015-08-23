dfe21fdda3b7905537fe8cbf5ac4f0d8dd8e8196
yakovb <yakov.boglev@gmail.com>
2015-08-23 11:55:22 +0100

    Added linear filter transformation

--- src/main/scala/operations/Transformation.scala
+++ src/main/scala/operations/Transformation.scala
@@ -14,5 +14,13 @@ case class TransformSimple[A,B](image: Image[A],
     val newMat: ParArray[B] = traversal traverse (image, pointOp)
     ParImage(newMat, image.width, image.height)
   }
+}
+
+case class TransformNeighbourhood[A,B](image: Image[A],
+                                        traversal: NeighbourTraverse,
+                                        neighbourOp: NeighbourhoodOperation[A,B]) extends Transformation {
+
+  def transform: Image[B] =
+    traversal traverse (image, neighbourOp)
 
 }
