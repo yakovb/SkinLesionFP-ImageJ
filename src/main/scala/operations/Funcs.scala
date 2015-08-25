eb3ba82c1afe872cb8f44bed8bed9bd8be41bdc6
yakovb <yakov.boglev@gmail.com>
2015-08-25 17:59:29 +0100

    Attempt to clean up convolution operation

--- src/main/scala/operations/Funcs.scala
+++ src/main/scala/operations/Funcs.scala
@@ -1,12 +1,14 @@
 package operations
 
 object Funcs {
+
+  def byte_2_float(b: Byte) = (b & 0xff).toFloat
+
   def rgb_2_grey: PointOp_3Channel[Double, Byte] =
     PointOp_3Channel[Double,Byte](_ * 0.2126, _ * 0.7152, _ * 0.0722) ((r,g,b) => (r+g+b).toByte)
 
-  def id_filter: LinearFilter[Byte, Int, Byte] =
-    LinearFilter[Byte,Int,Byte]((pairs: List[(Byte,Int)]) =>
-      (pairs map (p => p._1 * p._2)).sum.toByte)(List(0,0,0,0,1,0,0,0,0))
+  def id_filter  =
+    LinearFilter[Byte,Byte](byte_2_float, List(0,0,0,0,1,0,0,0,0), _.toByte)
 
   def simple_binary: PointOp[Byte, Byte] =
     PointOp[Byte,Byte]( (p:Byte) =>
@@ -16,9 +18,8 @@ object Funcs {
   def invert: PointOp[Byte, Byte] =
     PointOp[Byte,Byte]( (p:Byte) => (255 - p).toByte )
 
-  def gaussBlur: LinearFilter[Byte,Int,Byte] =
-    LinearFilter[Byte,Int,Byte]((pairs: List[(Byte,Int)]) =>
-      ((pairs map (p => (p._1 & 0xff) * p._2)).sum * (1.0 / 16)).toByte) (List(1,2,1,2,4,2,1,2,1))
+  def gaussBlur =
+    LinearFilter[Byte,Byte](byte_2_float, List(1,2,1,2,4,2,1,2,1), _.toByte)
 
   def medianFilter: NonLinearFilterNoKernel[Byte,Byte] =
     NonLinearFilterNoKernel[Byte,Byte](region => {
@@ -28,20 +29,14 @@ object Funcs {
       else sorted(l / 2)
     })
 
-  def rgb2xyz: PointOp_3Channel[Double, Array[Double]] =
-    PointOp_3Channel[Double,Array[Double]](
-      (r:Int) => {
-        val rr = r / 255
-        if (rr > .04045) Math.pow((rr + .055) / 1.055, 2.4)
-        else rr / 12.92},
-      (g:Int) => {
-        val gg = g / 255
-        if (gg > .04045) Math.pow((gg + .055) / 1.055, 2.4)
-        else gg / 12.92},
-      (b:Int) => {
-        val bb = b / 255
-        if (bb > .04045) Math.pow((bb + .055) / 1.055, 2.4)
-        else bb / 12.92})(Array(_,_,_))
+  def rgb2xyz_Op: PointOp[Int, Double] =
+    PointOp[Int, Double]((pixel: Int) => {
+        val temp = pixel / 255
+        if (temp > .04045) Math.pow((temp + .055) / 1.055, 2.4)
+        else temp / 12.92})
+
+//  def rgb2xyz_Combiner(x: Int, y: Int, z: Int) =
+
 }
 
 object BorderAction extends Enumeration {
