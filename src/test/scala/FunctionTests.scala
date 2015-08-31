afb98243f9154b1c12dba679c5adb8b9cfd459c9
yakovb <yakov.boglev@gmail.com>
2015-08-31 20:54:41 +0100

    Added 3channel test

--- src/test/scala/FunctionTests.scala
+++ src/test/scala/FunctionTests.scala
@@ -1,5 +1,5 @@
 import images.{Image, ParImage}
-import operations.{PointOp_1Channel, PointTraverse, TransformSimple}
+import operations.{PointOp_1Channel, PointOp_3Channel, PointTraverse, TransformSimple}
 import org.scalatest.prop.TableDrivenPropertyChecks
 import org.scalatest.{Matchers, PropSpec}
 
@@ -7,12 +7,22 @@ class FunctionTests extends PropSpec with TableDrivenPropertyChecks with Matcher
   val imageList = (new Matrices).getIntImages
   val images = Table("images", imageList : _*)
 
-  property("point op '+1' on all-zeroes image should give all-ones image") {
+  property("point op '+1' should result in sum of array + length of array") {
     forAll(images) { (image: Image[Int]) =>
       val newImage: ParImage[Int] = TransformSimple[Int,Int](image, PointTraverse(), PointOp_1Channel(_ + 1)) transform;
       newImage.matrix.sum should equal (image.matrix.sum + image.matrix.length)
     }
   }
 
+  property("point op 3-channel (where the channel is split up and recomposed) should result in original image") {
+    forAll(images) { image =>
+      val newImage = TransformSimple[Int,Int](image, PointTraverse(), PointOp_3Channel(
+        r => 1,
+        g => 1,
+        b => 1)
+        ((r:Int,g:Int,b:Int) => (r + g + b).toByte)) transform;
+      newImage.matrix should equal (Array.fill(400)(3))
+    }
+  }
 
 }
