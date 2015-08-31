3dc77a4c2366e9381a5f6f11e63700766ad26abd
yakovb <yakov.boglev@gmail.com>
2015-08-31 14:21:15 +0100

    Added kernel class

--- src/main/scala/images/Image.scala
+++ src/main/scala/images/Image.scala
@@ -13,3 +13,9 @@ case class ParImage[A](pixels: ParArray[A], w: Int, h: Int) extends Image[A] {
   override val height: Int = h
   override val matrix: ParArray[A] = pixels
 }
+
+case class Kernel[A](pixels: ParArray[A], w: Int, h: Int) extends Image[A] {
+  override val width: Int = w
+  override val height: Int = h
+  override val matrix: ParArray[A] = pixels
+}
