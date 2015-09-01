de9eb2ba0223aa2d7858b87900f125bd09948add
yakovb <yakov.boglev@gmail.com>
2015-09-01 20:19:39 +0100

    Added parameter error checking to Image and Kernel

--- src/main/scala/images/Image.scala
+++ src/main/scala/images/Image.scala
@@ -9,12 +9,22 @@ sealed trait Image[A] {
 }
 
 case class ParImage[A](pixels: ParArray[A], w: Int, h: Int) extends Image[A] {
+  require(pixels.nonEmpty, "cannot pass empty pixel array")
+  require(w != 0, "cannot have image with no width")
+  require(h != 0, "cannot have image with no height")
+  require(w * h == pixels.length, "check arguments: width * height should equal pixel array length")
+
   override val width: Int = w
   override val height: Int = h
   override val matrix: ParArray[A] = pixels
 }
 
 case class Kernel(pixels: List[Float], w: Int, h: Int) extends Image[Float] {
+  require(pixels.nonEmpty, "cannot pass empty pixel array")
+  require(w != 0, "cannot have image with no width")
+  require(h != 0, "cannot have image with no height")
+  require(w * h == pixels.length, "check arguments: width * height should equal pixel array length")
+
   override val width: Int = w
   override val height: Int = h
   override val matrix: ParArray[Float] = pixels.toParArray
