66adb52b203a8c6d32170bc001c0b754ca3a9b20
yakovb <yakov.boglev@gmail.com>
2015-09-01 19:54:36 +0100

    corrected methods to conform to new signatures

--- src/main/scala/plugins/Median_Filter.scala
+++ src/main/scala/plugins/Median_Filter.scala
@@ -1,21 +1,21 @@
 import ij.ImagePlus
 import ij.plugin.filter.PlugInFilter
 import ij.plugin.filter.PlugInFilter._
-import ij.process.{ByteProcessor, ImageProcessor}
+import ij.process.{ColorProcessor, ImageProcessor}
 import images.ParImage
-import operations.{Funcs, NeighbourTraverse, TransformNeighbourhood}
 
 class Median_Filter extends PlugInFilter {
   override def setup(arg: String, imp: ImagePlus): Int =
-  DOES_8G + DOES_STACKS + SUPPORTS_MASKING
+  DOES_ALL
 
   override def run(ip: ImageProcessor): Unit = {
-    val pixels = ip.getPixels.asInstanceOf[Array[Byte]]
-    val image = ParImage[Byte](pixels.par, ip.getWidth, ip.getHeight)
+    import operations.MyPipeline._
+    val pixels = ip.getPixels.asInstanceOf[Array[Int]]
+    val image = ParImage[Int](pixels.par, ip.getWidth, ip.getHeight)
 
-    val transformedImage = TransformNeighbourhood[Byte,Byte](image, NeighbourTraverse(), Funcs.medianFilter) transform
+    val transformedImage = transformToThreeChannel(image).transform
 
-    val result = new ByteProcessor(transformedImage.width, transformedImage.height, transformedImage.matrix.toArray)
+    val result = new ColorProcessor(transformedImage.width, transformedImage.height, transformedImage.matrix.toArray)
     new ImagePlus("grey pic", result) show()
   }
 }
