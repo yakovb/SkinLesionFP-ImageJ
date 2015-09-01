3f492f1692a798a9b32ce93947cdf767d83e4b0f
yakovb <yakov.boglev@gmail.com>
2015-09-01 20:52:55 +0100

    better specified Image preconditions

--- src/main/scala/images/Image.scala
+++ src/main/scala/images/Image.scala
@@ -10,8 +10,8 @@ sealed trait Image[A] {
 
 case class ParImage[A](pixels: ParArray[A], w: Int, h: Int) extends Image[A] {
   require(pixels.nonEmpty, "cannot pass empty pixel array")
-  require(w != 0, "cannot have image with no width")
-  require(h != 0, "cannot have image with no height")
+  require(w > 0, "cannot have image with no width")
+  require(h > 0, "cannot have image with no height")
   require(w * h == pixels.length, s"check arguments: width * height should equal pixel array length, but you have $w * $h = ${pixels.length}")
 
   override val width: Int = w
@@ -21,8 +21,8 @@ case class ParImage[A](pixels: ParArray[A], w: Int, h: Int) extends Image[A] {
 
 case class Kernel(pixels: List[Float], w: Int, h: Int) extends Image[Float] {
   require(pixels.nonEmpty, "cannot pass empty pixel array")
-  require(w != 0, "cannot have image with no width")
-  require(h != 0, "cannot have image with no height")
+  require(w > 1, "kernel width must be greater than 1, otherwise use a point operation")
+  require(h > 1, "kernel height must be greater than 1, otherwise use a point operation")
   require(w * h == pixels.length, s"check arguments: width * height should equal pixel array length, but you have $w * $h = ${pixels.length}")
 
   override val width: Int = w
