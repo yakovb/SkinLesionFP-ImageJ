5125b9776a1acfd08ad7e9843daa425d1bdc0eed
yakovb <yakov.boglev@gmail.com>
2015-09-02 23:56:42 +0100

    cleanup

--- src/main/scala/dermatological/binary_ops/Moments.scala
+++ src/main/scala/dermatological/binary_ops/Moments.scala
@@ -14,7 +14,8 @@ object Moments {
   val mFuncList = List(m00,m10,m01,m11,m20,m02)
   val mNames = List("m00", "m10", "m01", "m11", "m20", "m02")
 
-  def computeMoments(image: Image[Byte]) = MomentTraverse() traverse (image, mFuncList, mNames)
+  def getCentralMoments = (im: Image[Byte]) =>
+    MomentTransform(im, MomentTraverse()) transform
 }
 
 
@@ -27,7 +28,7 @@ case class MomentTraverse() extends Traversal {
     momentNames zip aggrData toMap
   }
   
-  def centralMoments(im: Image[Byte], centroidX: Int, centriodY: Int, 
+  def centralMoments(im: Image[Byte], centroidX: Double, centriodY: Double,
                      centralFuncs: List[(Int,Int) => Double], centralNames: List[String]) = {
     val rawData = loop(im, centralFuncs)
     val aggrData = for (i <- centralNames.indices) yield rawData map (lst => lst(i)) sum
@@ -45,17 +46,18 @@ case class MomentTraverse() extends Traversal {
 }
 
 
-case class MomentTransform() extends Transformation {
+case class MomentTransform(im: Image[Byte], mTraverse: MomentTraverse) extends Transformation {
   import Moments._
   
-  def transform(im: Image[Byte], mTraverse: MomentTraverse) = {
+  def transform = {
     val moments = mTraverse traverse (im, mFuncList, mNames)
-    val centX = (moments("m10").toFloat / moments("m00").toFloat).toInt
-    val centY = (moments("m01").toFloat / moments("m00").toFloat).toInt
+    val centX = moments("m10").toFloat / moments("m00").toDouble
+    val centY = moments("m01").toFloat / moments("m00").toDouble
 
-    val c10 = (r: Int, c: Int) => (c - centX).toDouble
-    val c01 = (r: Int, c: Int) => (r - centY).toDouble
-    val c11 = (r: Int, c: Int) => (c - centX).toDouble * (r - centY).toDouble
+    println(s"cx: $centX, cy: $centY")
+    val c10 = (r: Int, c: Int) => c - centX
+    val c01 = (r: Int, c: Int) => r - centY
+    val c11 = (r: Int, c: Int) => (c - centX) * (r - centY)
     val c20 = (r: Int, c: Int) => Math.pow(c - centX, 2)
     val c02 = (r: Int, c: Int) => Math.pow(r - centY, 2)
     val centMomList = List(c10, c01, c11, c20, c02)
