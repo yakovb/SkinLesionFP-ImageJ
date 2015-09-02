2a3d39fdc8c0928bb0a419838f63a66c20fee9e6
yakovb <yakov.boglev@gmail.com>
2015-09-02 16:30:03 +0100

    RGB to CIELab plugin

---
+++ src/main/scala/plugins/RGB_to_CIELab.scala
@@ -0,0 +1,20 @@
+import ij.plugin.filter.PlugInFilter
+import ij.plugin.filter.PlugInFilter._
+import ij.process.ImageProcessor
+import ij.{IJ, ImagePlus}
+import operations.{InteropImageJ, MyPipeline}
+
+class RGB_to_CIELab extends PlugInFilter {
+  override def setup(arg: String, imp: ImagePlus): Int =
+  DOES_ALL + DOES_STACKS
+
+  override def run(ip: ImageProcessor): Unit = {
+    import MyPipeline._
+    val src = InteropImageJ.getIntParImage(ip)
+    val pipeline = rgb_to_xyz andThen xyz_to_Lab
+    val Lab = pipeline (src)
+    val result = InteropImageJ.arrayFloatImage_to_IJstack("CIELab", Lab)
+    result show()
+    IJ.resetMinAndMax()
+  }
+}
