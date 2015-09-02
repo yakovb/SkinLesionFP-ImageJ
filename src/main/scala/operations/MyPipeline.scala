2f50863a081b0c79bed1770317f2992d192371b7
yakovb <yakov.boglev@gmail.com>
2015-09-02 21:34:36 +0100

    todo list

--- src/main/scala/operations/MyPipeline.scala
+++ src/main/scala/operations/MyPipeline.scala
@@ -1,5 +1,21 @@
 package operations
 
+//TODO moments
+//TODO centroid
+//TODO orientation
+//TODO rotation
+//TODO binary top, bottom, overlap
+//TODO asymmetry (overlap / total)
+
+//TODO floodfill
+//TODO filter non-central blobs
+//TODO make mask
+//TODO perimeter (4*pi*area / perimeter^2)
+
+//TODO mask on colour image
+//TODO split to three channel
+//TODO variance (and std dev) of each channel
+
 import images.{Image, ParImage}
 
 object MyPipeline {
