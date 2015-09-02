9bde97d169f1beeb166f06f7574f80baa97ace59
yakovb <yakov.boglev@gmail.com>
2015-09-02 09:40:23 +0100

    Updated pipeline for median filter

--- src/main/scala/operations/MyPipeline.scala
+++ src/main/scala/operations/MyPipeline.scala
@@ -3,11 +3,38 @@ package operations
 import images.Image
 
 object MyPipeline {
-  def getRed = PointOp_1Channel((pixel: Int) => (pixel >> 16) & 0xff)
+  def getRed(pixel: Int) = (pixel >> 16) & 0xff
+  def getGreen(pixel: Int) = (pixel >> 8) & 0xff
+  def getBlue(pixel: Int) = pixel & 0xff
+  def combineRgb(rgbList: List[Int]) = rgbList match {
+    case List(red,green,blue) =>
+      val r = (red << 16) & 0xff
+      val g = (green << 8) & 0xff
+      val b = blue  & 0xff
+      r + g + b
+  }
 
-  def getGreen = PointOp_1Channel((pixel: Int) => (pixel >> 8) & 0xff)
+  def medianOneChannel(region: List[Int]) = {
+    val sorted = region.sorted
+    val l = sorted.length
+    if (l % 2 == 0) (sorted(l / 2) + sorted(l / 2 + 1)) / 2
+    else sorted(l / 2)
+  }
 
-  def getBlue = PointOp_1Channel((pixel: Int) => pixel & 0xff)
+  def splitRgbRegion(region: List[Int]) = {
+    val reds = region map getRed
+    val greens = region map getGreen
+    val blues = region map getBlue
+    List(reds,blues,greens)
+  }
 
-  def transformToThreeChannel = TransformSimple(_: Image[Int], PointTraverse(), getRed,getGreen,getBlue)
+
+  def medianThreeChannel(mixedRegion: List[Int]) = {
+    val medians = splitRgbRegion(mixedRegion) map medianOneChannel
+    combineRgb(medians)
+  }
+
+  def medianOp = NonLinearFilterNoKernel(9, medianThreeChannel)
+
+  def medianFilter = TransformNeighbourhood(_: Image[Int], NeighbourTraverse(), medianOp) transform
 }
