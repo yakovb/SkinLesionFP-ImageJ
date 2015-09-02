8b44d8176164773753abdeafdb9597fd388bcc6b
yakovb <yakov.boglev@gmail.com>
2015-09-02 17:50:45 +0100

    functions to get median background

--- src/main/scala/operations/MyPipeline.scala
+++ src/main/scala/operations/MyPipeline.scala
@@ -88,4 +88,66 @@ object MyPipeline {
     def labOp = PointOp_1Channel(getLab)
     TransformSimple(_: Image[Array[Float]], PointTraverse(), labOp) transform
   }
+
+
+  def getBackgroundMedian =
+    TransformLabWindowsToMedian(_: Image[Array[Float]], TraverseBackgroundWindows(), MedianOfWindowsOp(labMedianCalc)) transform
+
+  case class TransformLabWindowsToMedian(image: Image[Array[Float]],
+                                         trav: TraverseBackgroundWindows,
+                                         op: NeighbourhoodOperation[List[Array[Float]], Array[Float]]) extends Transformation {
+    def transform: Array[Float] =
+      trav traverse (image, 10, op)
+  }
+
+
+  case class TraverseBackgroundWindows() extends Traversal {
+    def traverse(im: Image[Array[Float]],
+                 windowSize: Int,
+                 neighbourhoodOp: NeighbourhoodOperation[List[Array[Float]],Array[Float]]) = {
+
+      def getIndex(row: Int, col: Int) = im.width * row + col
+
+      def getCorner(rBegin: Int, rEnd: Int, cBegin: Int, cEnd: Int) = {
+        for {
+          r <- rBegin until rEnd
+          c <- cBegin until cEnd
+        } yield im.matrix(getIndex(r, c))
+      }
+
+      val upperLeft = getCorner(0, windowSize, 0, windowSize) toList
+      val upperRight = getCorner(0, windowSize, im.width - windowSize, im.width) toList
+      val lowerLeft = getCorner(im.height - windowSize, im.height, 0, windowSize) toList
+      val lowerRight = getCorner(im.height - windowSize, im.height, im.width - windowSize, im.width) toList
+      val allWindows = List(upperLeft, upperRight, lowerLeft, lowerRight)
+
+      neighbourhoodOp runOn allWindows
+    }
+  }
+
+  case class MedianOfWindowsOp(f: List[List[Array[Float]]] => Array[Float])
+    extends NeighbourhoodOperation[List[Array[Float]], Array[Float]] {
+    
+    override def runOn(neighbourhood: List[List[Array[Float]]]): Array[Float] =
+      f(neighbourhood)
+  }
+
+  def labMedianCalc(windows: List[List[Array[Float]]]) = {
+    val all_Ls = getArrayElems(windows, 0) sorted
+    val all_as = getArrayElems(windows, 1) sorted
+    val all_bs = getArrayElems(windows, 2) sorted
+
+    val median_L = all_Ls (all_Ls.length / 2)
+    val median_a = all_as (all_as.length / 2)
+    val median_b = all_bs (all_bs.length / 2)
+
+    Array(median_L, median_a, median_b)
+  }
+
+  private def getArrayElems(windows: List[List[Array[Float]]], index: Int) =
+    for {
+      window <- windows
+      labArray <- window
+    }  yield labArray(index)
+
 }
