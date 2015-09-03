5f8b056b11d656807c729243681bbb0b0185df79
yakovb <yakov.boglev@gmail.com>
2015-09-03 23:24:44 +0100

    plugin to remove non-central blobs

---
+++ src/main/scala/plugins/Kill_Small_Blobs.scala
@@ -0,0 +1,17 @@
+import dermatological.HolesAndSpecs
+import ij.ImagePlus
+import ij.plugin.filter.PlugInFilter
+import ij.plugin.filter.PlugInFilter._
+import ij.process.ImageProcessor
+import operations.InteropImageJ._
+
+class Kill_Small_Blobs extends PlugInFilter {
+  override def setup(arg: String, imp: ImagePlus): Int =
+    DOES_8G
+
+  override def run(ip: ImageProcessor): Unit = {
+    val src = getByteParImage(ip)
+    val cleaned = HolesAndSpecs.removeSpecs (src)
+    makeImagePlus("Single Blob", makeGreyProcessor(cleaned)) show()
+  }
+}
