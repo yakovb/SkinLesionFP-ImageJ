476c94e82b516afebf07c187efc315df7f35409a
yakovb <yakov.boglev@gmail.com>
2015-09-13 18:49:47 +0100

    preprocessing ScalaDoc

--- src/main/scala/dermatological/other_ops/PreProcessing.scala
+++ src/main/scala/dermatological/other_ops/PreProcessing.scala
@@ -1,25 +1,44 @@
 package dermatological.other_ops
 
-import core.{Image, NeighbourTraverse, NonLinearFilter, TransformNeighbourhood}
+import core._
 import dermatological.colour_ops.RGBaccess._
 
+/**
+ * Provides methods for pre-processing (i.e. noise removal) of images
+ */
 object PreProcessing {
 
-  def medianOneChannel(region: List[Int]) = {
+  /**
+   * Partially applied function; requires colour [[core.Image]] as input to complete
+   * @return [[core.Image]] with each colour channel passed through a median filter
+   */
+  def medianFilter: (Image[Int]) => Image[Int] = {
+    def medianOp = NonLinearFilter(3, medianThreeChannel)
+    TransformNeighbourhood(_: Image[Int], NeighbourTraverse(), medianOp) transform
+  }
+
+  /**
+   * Partially applied function; requires grey [[core.Image]] as input to complete
+   * @return [[core.Image]] with Gaussian blur of kernel size 3x3 applied
+   */
+  def gaussianBlurGreyImage: (Image[Byte]) => Image[Byte] = {
+    TransformNeighbourhood(_: Image[Byte], NeighbourTraverse(), gaussBlur) transform
+  }
+
+  private def medianThreeChannel(mixedRegion: List[Int]) = {
+    val medians = splitRgbRegion(mixedRegion) map medianOneChannel
+    combineRgbDirect(medians)
+  }
+
+  private def medianOneChannel(region: List[Int]) = {
     val sorted = region.sorted
     val l = sorted.length
     if (l % 2 == 0) (sorted(l / 2) + sorted(l / 2 + 1)) / 2
     else sorted(l / 2)
   }
 
-  def medianFilter = {
+  private def gaussBlur =
+    LinearFilter[Byte,Byte](Kernel(List(1,2,1,2,4,2,1,2,1),3,3), byte_2_float(), _.toByte, 1f/16)
 
-    def medianThreeChannel(mixedRegion: List[Int]) = {
-      val medians = splitRgbRegion(mixedRegion) map medianOneChannel
-      combineRgbDirect(medians)
-    }
-    def medianOp = NonLinearFilter(3, medianThreeChannel)
-    TransformNeighbourhood(_: Image[Int], NeighbourTraverse(), medianOp) transform
-  }
-
+  private def byte_2_float(b: Byte) = (b & 0xff).toFloat
 }
