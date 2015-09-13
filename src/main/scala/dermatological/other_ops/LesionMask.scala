9b3b425163c848621dce729497b5b25c36d334ca
yakovb <yakov.boglev@gmail.com>
2015-09-13 18:34:17 +0100

    Lesion mask ScalaDoc

--- src/main/scala/dermatological/other_ops/LesionMask.scala
+++ src/main/scala/dermatological/other_ops/LesionMask.scala
@@ -4,20 +4,28 @@ import core.{Image, ParImage, Transformation, Traversal}
 
 import scala.collection.parallel.immutable.ParSet
 
+/**
+ * Provides method for creating masked colour images
+ */
 object LesionMask {
-  val WHITE = 16777215
 
+  private val WHITE = 16777215
+
+  /**
+   * Partially applied function; requires colour [[core.Image]] and mask
+   * @return masked [[core.Image]] with [[scala.Int]] pixel array with all pixels not in the mask set to WHITE
+   */
   def maskColourImage = (im: Image[Int], mask: ParSet[(Int,Int)]) =>
     MaskTransform(im, OverlayMaskTraverse(), mask) transform
 
-  case class MaskTransform(im: Image[Int], traversal: OverlayMaskTraverse,  mask: ParSet[(Int,Int)]) extends Transformation {
+  private case class MaskTransform(im: Image[Int], traversal: OverlayMaskTraverse,  mask: ParSet[(Int,Int)]) extends Transformation {
     def transform = {
       val resultMatrix = traversal traverse (im, mask)
       ParImage(resultMatrix, im.width, im.height)
     }
   }
 
-  case class OverlayMaskTraverse() extends Traversal {
+  private case class OverlayMaskTraverse() extends Traversal {
 
     def traverse(im: Image[Int], mask: ParSet[(Int,Int)]) = {
       val result = for {
