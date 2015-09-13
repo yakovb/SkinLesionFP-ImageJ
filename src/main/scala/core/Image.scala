302294c9081ed57e01044dd7dbf4c499739f1f5e
yakovb <yakov.boglev@gmail.com>
2015-09-13 17:02:40 +0100

    Image ScalaDoc

--- src/main/scala/core/Image.scala
+++ src/main/scala/core/Image.scala
@@ -2,12 +2,23 @@ package core
 
 import scala.collection.parallel.mutable.ParArray
 
+/**
+ * Base trait encapsulating an image with a pixel array, a width and a height
+ * @tparam A
+ */
 trait Image[A] {
   val width: Int
   val height: Int
   val matrix: ParArray[A]
 }
 
+/**
+ * [[core.Image]] with an underlying parallelisable pixel array
+ * @param pixels
+ * @param w
+ * @param h
+ * @tparam A
+ */
 case class ParImage[A](pixels: ParArray[A], w: Int, h: Int) extends Image[A] {
   require(pixels.nonEmpty, "cannot pass empty pixel array")
   require(w > 0, "cannot have image with no width")
@@ -19,6 +30,12 @@ case class ParImage[A](pixels: ParArray[A], w: Int, h: Int) extends Image[A] {
   override val matrix: ParArray[A] = pixels
 }
 
+/**
+ * Representation of a convolution kernel where each matrix cell is a [[scala.Float]]
+ * @param pixels
+ * @param w
+ * @param h
+ */
 case class Kernel(pixels: List[Float], w: Int, h: Int) extends Image[Float] {
   require(pixels.nonEmpty, "cannot pass empty pixel array")
   require(w > 1, "kernel width must be greater than 1, otherwise use a point operation")
