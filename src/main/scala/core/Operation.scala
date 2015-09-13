d3699ad6b5c8d82ceb93dcf89f08f1ebf9ff486a
yakovb <yakov.boglev@gmail.com>
2015-09-13 13:01:53 +0100

    Point operation ScalaDoc

--- src/main/scala/core/Operation.scala
+++ src/main/scala/core/Operation.scala
@@ -1,18 +1,52 @@
 package core
 
+/** Base trait encapsulating pixel operations */
 trait Operation
 
+/** Base trait encapsulating operations on a single pixel of A, returning a B */
 trait PointOperation[-A,+B] extends Operation {
+
+  /**
+   * Abstract operation on a single pixel
+   * @param pixel pixel of type A
+   * @return pixel of type B
+   */
   def runOn(pixel: A): B
 }
 
+/**
+ * Point operation on a pixel that represents a single channel
+ * @param f function taking a pixel of A to a pixel of B
+ * @tparam A pixel of type A
+ * @tparam B pixel of type B
+ */
 case class PointOp_1Channel[A,B](f: A => B) extends PointOperation[A,B] {
+
+  /**
+   * Takes pixel of A to pixel of B
+   * @param pixel pixel of type A
+   * @return pixel of type B
+   */
   override def runOn(pixel: A): B = f(pixel)
 }
 
+/**
+ * Point operation on a pixel that represents three colour channels packed into an [[scala.Int]].
+ * @param redOp function processing red channel
+ * @param greenOp function processing green channel
+ * @param blueOp function processing blue channel
+ * @param combine function combining the results of processing the colour channels
+ * @tparam A intermediate result type of colour processing functions
+ * @tparam B resulting pixel type
+ */
 case class PointOp_3Channel[A,B](redOp: Int => A, greenOp: Int => A, blueOp: Int => A)
                                 (combine: (A,A,A) => B) extends PointOperation[Int,B] {
-  
+
+  /**
+   * Takes packed [[scala.Int]] to a pixel of B
+   * @param pixel pixel of type A
+   * @return pixel of type B
+   */
   override def runOn(pixel: Int): B = {
     val red = redOp ((pixel >> 16) & 0xff)
     val green = greenOp ((pixel >> 8) & 0xff)
