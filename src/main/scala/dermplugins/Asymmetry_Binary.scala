08742c4b0aa42d0ddad1391a2d4e210496618509
yakovb <yakov.boglev@gmail.com>
2015-09-13 17:46:19 +0100

    Asymmetry ScalaDoc

--- src/main/scala/dermplugins/Asymmetry_Binary.scala
+++ src/main/scala/dermplugins/Asymmetry_Binary.scala
@@ -5,6 +5,9 @@ import ij.plugin.filter.PlugInFilter
 import ij.plugin.filter.PlugInFilter._
 import ij.process.ImageProcessor
 
+/**
+ * [[ij.plugin.filter.PlugInFilter]] that shows asymmetrical region of a skin lesion
+ */
 class Asymmetry_Binary extends PlugInFilter {
   override def setup(arg: String, imp: ImagePlus): Int =
     DOES_8G
